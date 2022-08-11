a674ba2b90808bcb928459a01f4e4c76c0852024
Hermawan <agenghermawan16@gmail.com>
2022-08-11 12:54:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('/','/prototype/login');
+
+Route::prefix('prototype')->group(function() {
+    Route::get('/login', function(){
+        return Inertia::render('Prototype/Login');
+    });
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
+
+
+
+
+
+
+
+// Route::middleware('role:admin')->group(function (){
+//     Route::get('admin',function() {
+//         return 'hi admin';
+//     });
+// });
+
+// Route::middleware('role:user')->group(function (){
+//     Route::get('user',function() {
+//         return 'hi user';
+//     });
+// });
